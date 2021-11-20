76c4a9ee805a0ec1c66724d2556e40e4e6cbcd39
Srishtiv2001 <srishtiv.2019@vitstudent.ac.in>
2021-11-20 08:49:30 +0530

    commit on 'main'

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 from flask import Flask, render_template,request
 from gingerit.gingerit import GingerIt
+import nltk
+import pysbd, re 
 
 app = Flask(__name__)
+
+segmentor = pysbd.Segmenter(language="en", clean=False)
+subsegment_re = r'[^;:\n•]+[;,:\n•]?\s*'
+
+def runGinger(par):
+    fixed = []
+    cor={'corrections':[],'result':'','text':''}
+    for sentence in segmentor.segment(par):
+        if len(sentence) < 300:
+            fixed.append(GingerIt().parse(sentence)['result'])
+            if len(GingerIt().parse(sentence)['corrections'])>0:
+              cor['corrections'].append(GingerIt().parse(sentence)['corrections'])
+        else:
+            subsegments = re.findall(subsegment_re, sentence)
+            if len(subsegments) == 1 or any(len(v) < 300 for v in subsegments):
+                # print(f'Skipped: {sentence}') // No grammar check possible
+                fixed.append(sentence)
+            else:
+                res = []
+                for s in subsegments:
+                    res.append(GingerIt().parse(s)['result'])
+                    if len(GingerIt().parse(sentence)['corrections'])>0:
+                      cor['corrections'].append(GingerIt().parse(sentence)['corrections'])
+                fixed.append("".join(res))
+    cor['result']=" ".join(fixed)
+    cor['text']=par
+    return cor
 
 @app.route('/')
 def index():
@@ -9,13 +38,12 @@ def index():
 
 @app.route('/results',methods=['POST'])
 def result():
-    txt=request.form.get('description')
-    parser = GingerIt()
-    dic=parser.parse(txt)
-    num=len(dic["corrections"])
+    txt=request.form.get('description')                
+    yes = runGinger(txt)
+    num=len(yes['corrections'])
     if num==0:
-        dic={'corrections':'None','result':txt}
-    return render_template('second page.html',dic=dic,num=num)
+        yes={'corrections':'None','result':txt}
+    return render_template('second page.html',dic=yes,num=num)
 
 @app.route('/',methods=['POST'])
 def index2():
